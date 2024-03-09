fd4ef731310d40eb44dbd75d305d3824b37ac33d
SharmaS1402 <kusum9418177125@gmail.com>
2024-03-09 18:35:02 +0530

    Chaning contract address to sepolia testnet

--- practice/constants.js
+++ practice/constants.js
@@ -1,4 +1,4 @@
-export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const contractAddress = "0x7a1128C98395Be4dd1FC89656449245A333462C8";
 
 export const abi = [{
     "inputs": [
