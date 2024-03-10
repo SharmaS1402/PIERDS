14992c8c74e53959c013570ae57b3d54464aadee
Sheshank <22bcs102@nith.ac.in>
2024-03-10 11:51:38 +0530

    webcam button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
 
-import WebcamCapture from "./components/WebcamCapture";
-import Form from "./components/form";
-import NameAndButtons from "./components/don";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import NewPage from "./components/NewPage";
 import Organisation from "./components/Organisation";
+import WebcamCapture from "./components/WebcamCapture";
+import NameAndButtons from "./components/don";
+import Form from "./components/form";
 
 function App() {
   return (
@@ -15,7 +13,7 @@ function App() {
           <Route path="/" element={<Form />} />
           <Route path="/user" element={<NameAndButtons />} />
           <Route path="/organisation" element={<Organisation />} />
-          <Route path="/webcam" element={<WebcamCapture/>} />
+          <Route path="organisation/webcam" element={<WebcamCapture/>} />
           
         </Routes>
       </Router>
