8961162f3320bdbfb67108c15c73be64096bb858
SharmaS1402 <kusum9418177125@gmail.com>
2024-03-11 10:33:46 +0530

    updating app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-<<<<<<< HEAD
+
 import NewPage from "./components/NewPage";
+
 import Distri from "./components/Distri";
-=======
->>>>>>> f158bf5b0288cd9e83d5289722fdc983112c7bd9
+
 import Organisation from "./components/Organisation";
 import WebcamCapture from "./components/WebcamCapture";
 import NameAndButtons from "./components/don";
@@ -18,12 +18,10 @@ function App() {
           <Route path="/" element={<Form />} />
           <Route path="/user" element={<NameAndButtons />} />
           <Route path="/organisation" element={<Organisation />} />
-<<<<<<< HEAD
-          <Route path="/distri" element={<Distri />} />
-=======
+
           <Route path="organisation/webcam" element={<WebcamCapture/>} />
+          <Route path="organisation/webcam/next" element={<Distri />} />
           
->>>>>>> f158bf5b0288cd9e83d5289722fdc983112c7bd9
         </Routes>
       </Router>
     </>
